68cf4ee34df66e8b21c73ec8af18deb16ab28645
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-05-19 15:54:21 -0400

    replace jsdelivr with unpkg
    see https://github.com/jsdelivr/jsdelivr/issues/18397

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const sitemap = require("@njzjz/sitemap");
 const { htmlTag } = require("hexo-util");
 const path = require('path');
 const { name, version } = require("./package.json");
-const { npm_url } = require('jsdelivr_url');
+const { npm_url } = require('unpkg_url');
 const css = hexo.extend.helper.get('css').bind(hexo);
 
 hexo.extend.filter.register('theme_inject', function (injects) {
